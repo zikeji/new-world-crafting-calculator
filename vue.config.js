6b076bc841875d0983fd5a3a018f19530ed89e3f
Zikeji <me@zikeji.com>
2021-10-08 01:11:41 -0400

    style: alphabetize

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
   pages: {
     index: {
       entry: 'src/main.js',
       title: 'New World Crafting Calculator'
     }
-  }
+  },
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
